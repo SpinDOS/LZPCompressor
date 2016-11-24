ad962f472504bf1256e5acb3d18305d67c473a05
Alexander <Alexander@ALEXANDER-PC>
2016-11-24 11:21:57 +0300

    Remove empty usings

--- LZPCompressor/Program.cs
+++ LZPCompressor/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Security;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 
 // Реализовать компрессор для сжатия произвольных файлов на основе 
